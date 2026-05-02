8303bef5c56d0c1cceada486a57307b8a29c1276
LeastActionCompany <leastactioncompany@gmail.com>
2026-05-02 13:53:01 +0530

    udpated product page

--- src/pages/home/ProductsPage.js
+++ src/pages/home/ProductsPage.js
@@ -147,7 +147,7 @@ const ProductsPage = () => {
             </div>
           ) : products.length > 0 ? (
             <>
-              <div className="grid grid-cols-2 lg:grid-cols-3 gap-5">
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
                 {products.map(p => <ProductCard key={p._id} product={p} />)}
               </div>
               {canLoadMore && (
